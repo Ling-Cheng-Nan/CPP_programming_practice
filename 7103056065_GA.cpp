615efbfbd76002525a04f2a34e8adbad4b836c7e
Ling Cheng Nan <nan800130@gmail.com>
2020-03-28 19:42:12 +0800

    create rm delete xx

---
+++ 7103056065_GA.cpp
@@ -0,0 +1,40 @@
+/*
+Author : 凌政楠
+Create Date : 2020/03/28
+Program : Metaheuristic Algorithms
+Assignment : implementation of Genetic Algorithm
+Problem Description : 
+    
+    while(condition)
+     |   
+     |   1. Initial population
+     |   2. Fitness function
+     |   3. Selection
+     |   4. Crossover
+     |   5. Mutation
+     |
+    repeat step 1 - 5
+*/
+
+#include <iostream>
+
+/* directives */
+using namespace std ; //group different code to prevent naming conflicts
+
+/* function prototypes */
+
+int main(){
+
+    /*variables declaration*/
+    
+
+
+    return 0;
+}
+
+
+/* function implementation */
+void Selection(){
+
+}
+
--- firstcpp.cpp
+++ firstcpp.cpp
@@ -3,22 +3,19 @@
 //directives
 using namespace std ; //group different code to prevent naming conflicts
 
-void test();
-
 int main(){
 
     /*variables*/
-    int slice = 5;
+    int slice ;
     double aa = 10.3;
-    bool flag = false;
+    bool flag  = false;
+    string str = ""; 
 
-    std::cout<<"Hello World!\n";
-    test();
-
-    cout << slice ;
+    std::cout<<"Hello World!\n"; 
+    cout << "Enter the number of pizza you want to eat: " << endl;
+    cin  >> slice;
+    cout << "You have " << slice << " slices of pizza." << endl ;
+    
+    printf("printf print out %d slices \n", slice);
     return 0;
-}
-
-void test(){
-    cout << "Hello World!!! in test function\n";
 }
