9d8bc0b1a348debf97a1f3d506b81de4d88415e8
Ling-Cheng-Nan <nan800130@gmail.com>
2020-04-03 11:06:16 +0800

    dd

--- io.cpp
+++ io.cpp
@@ -4,35 +4,54 @@
 
 int main(){
 
-    std::ofstream myfile;
-    std::vector <std::string> names;
+    // std::ofstream myfile;
+    // std::vector <std::string> names;
 
-    std::string filename;
-    std::cin >> filename;   
+    // std::string filename;
+    // std::cin >> filename;   
 
-    myfile.open(filename.c_str(), std::ios::app);
-    // myfile << "Hello~ Sam" << std::endl;
-    try{
-        if(myfile.is_open()){
-            std::cout << "open file success n00b.\n";
-        }else
-        {
-            throw "File open failed.";
+    // myfile.open(filename.c_str(), std::ios::app);
+    // // myfile << "Hello~ Sam" << std::endl;
+    // try{
+    //     if(myfile.is_open()){
+    //         std::cout << "open file success n00b.\n";
+    //     }else
+    //     {
+    //         throw "File open failed.";
+    //     }
+        
+    // }
+    // catch(const char * err){
+    //     std::cout << err << std::endl;
+    // }
+    // names.push_back("SAM");
+    // names.push_back("MAX");
+    // names.push_back("JIN");
+
+    // for(std::string name : names){
+    //     myfile << name << std::endl;
+    // }
+
+    // myfile.close();
+
+    std::vector <std::string> names2;
+    std::ifstream infile("taco.txt");
+    std::string input;
+    // std::string inputstr;
+    // while(infile >> inputstr){
+    //     names2.push_back(inputstr);
+    // }
+
+    // for(std::string name : names2){
+    //     std::cout << name << std::endl;
+    // }
+    // std::ifstream infile("taco2.txt");
+    
+        if(infile.is_open){
+            std::cout << "file opened successful." << endl;
+            getline(infile, input);
+            std::cout << input << std::endl;
         }
         
-    }
-    catch(const char * err){
-        std::cout << err << std::endl;
-    }
-    names.push_back("SAM");
-    names.push_back("MAX");
-    names.push_back("JIN");
-
-    for(std::string name : names){
-        myfile << name << std::endl;
-    }
-
-    myfile.close();
-
     return 0;
 }
