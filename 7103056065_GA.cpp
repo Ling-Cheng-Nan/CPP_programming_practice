0e179b26b596b248e2b4241324a7dcafe6dcc0ae
Ling Cheng Nan <nan800130@gmail.com>
2020-04-06 12:12:03 +0800

    d

--- 7103056065_GA.cpp
+++ 7103056065_GA.cpp
@@ -18,9 +18,12 @@ Problem Description :
 
 #include <iostream>
 #include <string>
+#include <cmath>
+#include <ctime>
+
 #define AmountOfPopulation 4 //子代數量
 #define LengthOfChromosome 8 //染色體長度 (how many bits to encoded)
-
+#define MinimumSelected AmountOfPopulation/2
 
 /* directives */
 using namespace std ; //group different code to prevent naming conflicts
@@ -29,7 +32,7 @@ using namespace std ; //group different code to prevent naming conflicts
 void showPopulation(int [][LengthOfChromosome+1]);
 void initial_population(int [][LengthOfChromosome+1]);
 void fitness_function(int [][LengthOfChromosome+1]);
-void selection();
+void selection(int [][LengthOfChromosome+1]);
 void crossover();
 void mutation();
 
@@ -37,40 +40,96 @@ void mutation();
 int main(){
 
     /*variables declaration*/
+    srand(time(NULL));
     int cut_index = 0;
     int str[AmountOfPopulation][LengthOfChromosome+1];
-    
-    //string *pstr = &str;
+
+
     initial_population(str);
     showPopulation(str);
     fitness_function(str);
     showPopulation(str);
+    selection(str);
+
     return 0;
 }
 
 /* function implementation */
 void showPopulation(int str[][LengthOfChromosome+1]){
+    cout << "  gene  | fitness" << endl;
     for(int j = 0 ; j < AmountOfPopulation ; j++){
         for(int i = 0 ; i < LengthOfChromosome+1 ; i++){
-            cout << str[j][i];
+            if(i==LengthOfChromosome)
+                cout << "|    " << str[j][i]  << endl;
+            else
+                cout << str[j][i];
+
         }
-        cout << endl;
+        // cout << endl;
     }
+    cout << endl;
+
 }
 
 void initial_population(int str[][LengthOfChromosome+1]){
+    
     cout << "...Initial Population..." << endl;
     for(int j = 0 ; j < AmountOfPopulation ; j++){
         for(int i = 0 ; i < LengthOfChromosome+1 ; i++){
-            str[j][i] = rand()%2;
+            if( i == LengthOfChromosome ){
+                str[j][i] = 0;
+            }
+            else{
+                str[j][i] = rand()%2;
+            }
         }
     }
 }
 
 void fitness_function(int str[][LengthOfChromosome+1]){
     
+    cout << "...Fitness Fitness..." << endl;
+    int fitness[LengthOfChromosome]={0};
+    for(int j = 0 ; j < AmountOfPopulation ; j++){
+        for(int i = 0 ; i <= LengthOfChromosome-1 ; i++){
+           fitness[j] += (str[j][i])*pow(2, (LengthOfChromosome-1)-i);
+        }
+
+        str[j][LengthOfChromosome] = fitness[j];
+        // cout << str[j][LengthOfChromosome] << endl;
+    }
     
 }
-void selection(){}
-void crossover(){}
-void mutation(){}
+
+void selection(int str[][LengthOfChromosome+1]){
+    int selected[MinimumSelected][LengthOfChromosome+1] = {0};
+    bool usedindex[AmountOfPopulation] = {0};
+    
+    int min = pow(2, LengthOfChromosome);
+            
+    for(int j = 0 ; j < MinimumSelected ; j++){
+    
+        for(int i = 0 ; i < AmountOfPopulation ; i++){
+            if(usedindex[i] == false && str[i][LengthOfChromosome] < min){
+                min = str[i][LengthOfChromosome];
+                
+            }
+    
+        }
+
+        cout << min << endl;
+    }
+}
+
+void crossover(int str[][LengthOfChromosome+1],int cut){
+    
+}
+
+void mutation(int str[][LengthOfChromosome+1]){
+    /*generate mutation index*/
+    int mutation_num = rand()%AmountOfPopulation+1;
+    int mutation_gene = rand()%AmountOfPopulation;
+    int mutation_index = rand()%LengthOfChromosome;
+
+
+}
