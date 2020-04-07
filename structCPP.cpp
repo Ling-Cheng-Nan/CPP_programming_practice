7e13dc11a9dac5639a927b34158d0164f9da482f
Ling Cheng Nan <nan800130@gmail.com>
2020-04-07 18:21:41 +0800

    sss

--- structCPP.cpp
+++ structCPP.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 #include <string>
 
+struct User{
+    std::string first_name;
+    std::string  last_name;
+    std::string status;
+
+    
+};
+
 int main(){
+
     
     return 0;
 }
