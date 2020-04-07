f5313f3499231bfcc0951657b2afc91290e8f918
Ling Cheng Nan <nan800130@gmail.com>
2020-04-07 18:20:09 +0800

    finish tutorial 86

--- oop/oop.cpp
+++ oop/oop.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 class User{
     private:
@@ -12,20 +14,59 @@ class User{
             return status;
         }
 
-    // std::string setStatus(std::string sta){
-    //     status = sta;
-    // }
-
+        void setStatus(std::string str){
+            status = str;
+        }
 };
+
+int add_user_if_not_exists(std::vector<User> &users, User user){
+    
+    for(int i = 0 ; i < users.size() ; i++){
+        /*if match, return index i */
+        if((users[i].first_name == user.first_name) && 
+           (users[i].last_name == user.last_name)){
+            return i;
+        }
+    }
+    users.push_back(user);
+
+    return users.size()-1;
+}
 
 int main(){
 
-    User me;
-    me.first_name = "Sam";
-    me.last_name = "Ling";
-    // me.setStatus("Gold");
+    User user1, user2 , user3, user4, user5;
+    user1.first_name = "Sam";
+    user1.last_name  = "Ling";
+
+    user2.first_name = "Sam";
+    user2.last_name  = "Cheng";
     
-    me.getStatus();
+    user3.first_name = "Sammmy";
+    user3.last_name  = "Lou";    
+    
+    user4.first_name = "Max";
+    user4.last_name  = "Smith";
+    
+    user5.first_name = "Sammmy";
+    user5.last_name  = "Lou";
+    
+    std::vector<User> users;
+    users.push_back(user1);
+    users.push_back(user2);
+    users.push_back(user3);
+    users.push_back(user1);
+
+    std::cout << users.size() << std::endl;
+    std::cout << add_user_if_not_exists(users, user4) << std::endl;
+    std::cout << users.size() << std::endl;
+    std::cout << add_user_if_not_exists(users, user5) << std::endl;
+    std::cout << users.size() << std::endl;
+
+
+
+    // std::cout << users[0].first_name + users[0].last_name << std::endl;
+
 
     return 0;
 }
